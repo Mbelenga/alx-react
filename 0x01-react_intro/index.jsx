a5772fe4135b490e50546b21b650584756b69f7d
Mbelenga <mbelengaterence@gmail.com>
2024-11-27 23:08:45 +0300

    rendering some markup to the root

---
+++ 0x01-react_intro/index.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client"
+
+// creating a root
+const root = createRoot(document.getElementById("root"))
+// Render some markup to the root
+root.render(<h1>Hello, React</h1>)
