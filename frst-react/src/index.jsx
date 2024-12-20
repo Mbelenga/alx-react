1cd27847128c16d325dde3f97a42320a5f603329
Mbelenga <mbelengaterence@gmail.com>
2024-12-20 17:02:04 +0300

    updated

--- frst-react/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client"
-
-createRoot(document.getElementById("react")).render(
-    <ul>
-        <li> Super Popular Javascript library </li>
-        <li> Will help me be even more employable </li>
-        <li> React has a really cool logo </li>
-    </ul>
-)
