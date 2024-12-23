99491c7981f000c0525822e2ed83bfd3a1714e28
Mbelenga <mbelengaterence@gmail.com>
2024-12-24 01:08:38 +0300

    updated

--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = 20
+const myFirstElement = <h1>Hello React!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(myFirstElement)
