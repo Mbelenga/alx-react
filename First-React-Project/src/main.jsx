5535c9c209241798dc662234a645dc8eaf368c17
Mbelenga <mbelengaterence@gmail.com>
2024-12-28 00:19:57 +0300

    updated

--- First-React-Project/src/main.jsx
+++
@@ -1,9 +0,0 @@
-
- import App from './App.jsx';
- import './index.css';
- import { createRoot } from 'react-dom/client';
-
- const root = createRoot(document.getElementById("root"))
- root.render(
-  <h1> Hello from React</h1>
- )
