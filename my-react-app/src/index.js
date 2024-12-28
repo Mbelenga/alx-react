a89a8e068d7524d2625aecedd588c28332d3d750
Mbelenga <mbelengaterence@gmail.com>
2024-12-28 17:49:26 +0300

    updated

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -5,6 +5,7 @@ const App = () => {
     return (
         <div>
             <h1>Hello, React!</h1>
+            <p>React is a JavaScript library for building user interfaces.</p>
         </div>
     );
 };
