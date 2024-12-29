67faa5d892b2c3015b802b39132beba677390652
Mbelenga <mbelengaterence@gmail.com>
2024-12-29 03:29:45 +0300

    succesfully created a react component

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'; // Import React
-import ReactDOM from 'react-dom/client'; // Import ReactDOM for rendering
-import App from './App'; // Import the App component
-import './index.css'; // Import global styles
-
-// Mount the React app to the root div in index.html
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
