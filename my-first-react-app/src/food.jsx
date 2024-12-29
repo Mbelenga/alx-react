86aeb31cef4e6215b88daa3d27e6c834c38e8745
Mbelenga <mbelengaterence@gmail.com>
2024-12-29 03:36:00 +0300

    updated

---
+++ my-first-react-app/src/food.jsx
@@ -0,0 +1,12 @@
+function Food() {
+  return (
+    <div>
+      <h1> My Favourite Food</h1>
+      <ul>
+        <li>Pizza</li>
+        <li>Orange</li>
+        <li>Banana</li>
+      </ul>
+    </div>
+  );
+}
