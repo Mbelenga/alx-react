f060ce7a3996aaa0b8bbdfc6846e3f2f1f6d2b1d
Mbelenga <mbelengaterence@gmail.com>
2024-12-31 01:55:40 +0300

    A component that lists multiple A nimals

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Greeting from './Greetings';
 import Food from './food';
+import Animal from './Animals';
 
 function App() {
     return (
         <div>
             <Greeting />
             <Food />
+            <Animal />
         </div>
     );
 }
