facd3dad1c06f58b6a03cca866ecce8f73cd5a23
Mbelenga <mbelengaterence@gmail.com>
2024-12-31 01:53:01 +0300

    updated

---
+++ my-first-react-app/src/Animals.jsx
@@ -0,0 +1,13 @@
+function Animal() {
+    return (
+        <div>
+            <h1>Animal: </h1>
+            <ul>
+                <li>Monkey</li>
+                <li>Lion</li>
+                <li>Elephant</li>
+                <li>Snake</li>
+            </ul>
+        </div>
+    );
+}
