2a6d365bfddca438ba8154fb53c5497852ebec22
Mbelenga <mbelengaterence@gmail.com>
2025-01-22 02:27:57 +0300

    creating a variable called bonusPoints and woking with it

--- Scrimba/Passenger_Couner_App/index.js
+++ Scrimba/Passenger_Couner_App/index.js
@@ -1,7 +1,17 @@
 // document.getElementById("count-el").innerText = 5
 
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch
+// let count = 5
+// count =3
+// console.log(count)
 
-console.log(count)
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
