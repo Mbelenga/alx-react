52306c9ad3de50bfcaf49095fa4d5897464ed83d
Mbelenga <mbelengaterence@gmail.com>
2025-01-26 23:06:12 +0300

    correcting an error

--- Scrimba/Passenger_Couner_App/index.js
+++ Scrimba/Passenger_Couner_App/index.js
@@ -1,13 +1,13 @@
 // document.getElementById("count-el").innerText = 5
 
 // change the count-el in the HTML  to reflect the new count
-
-let countEL = document.getElementById("count-el")
-
 let count = 0
+
+let countEL = document.getElementById("count-el") // pass in arguments
+
+console.log(countEl)
 
 function increment() {
     count = count + 1
-
-    console.log(count)
+    countEL.innerText = count
 }
