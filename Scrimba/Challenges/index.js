93d6bbef0af684362c451989731364389318944f
Mbelenga <mbelengaterence@gmail.com>
2025-01-29 01:02:18 +0300

    writing a welcome message

--- Scrimba/Challenges/index.js
+++ Scrimba/Challenges/index.js
@@ -4,4 +4,6 @@ let name = "Terrence"
 let greeting = "How are you doing?"
 
 // Rendering the welcome message using welcomeEL.innertext
-welcomeEL.innerText = "Welcome back, " + name + "! " + greeting
+welcomeEL.innerText = greeting + name
+
+welcomeEL.innerText = welcomeEL.innerText + " 😀"
