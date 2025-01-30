0b2df6b61c8e3d88279df2408b1cd64b9ef90fd8
Mbelenga <mbelengaterence@gmail.com>
2025-01-31 00:59:43 +0300

    Javascript Amazes me

--- Scrimba/Challenges/main.js
+++ Scrimba/Challenges/main.js
@@ -1,2 +1,5 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello World";
+document.querySelector("html").addEventListener('click', function () {
+    alert("ouch! Stop poking me!")
+});
