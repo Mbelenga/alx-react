8360ae821546bd07a0f60014686765fd72b8341b
Mbelenga <mbelengaterence@gmail.com>
2025-02-06 02:17:58 +0300

    rendering an error message using javascript

--- Scrimba/Challenges/shoes.js
+++ Scrimba/Challenges/shoes.js
@@ -1,3 +1,11 @@
 // When th user clicks the purchase button, reder out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error"
+
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph)
+
+function purchse() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
