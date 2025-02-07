1eb97291821d1e47f3b7a41a3766ca22648b11b6
Mbelenga <mbelengaterence@gmail.com>
2025-02-07 19:18:19 +0300

    updated

---
+++ Scrimba/Challenges/calculator.js
@@ -0,0 +1,8 @@
+let num1 = 10
+let num2 = 22
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+function add() {
+    console.log("The add button is clicked")
+}
