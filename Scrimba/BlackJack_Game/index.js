dd043a32d36eacfb00bd4fb2dc8e37c4f7c311ed
Mbelenga <mbelengaterence@gmail.com>
2025-02-19 00:10:09 +0300

    updated

--- Scrimba/BlackJack_Game/index.js
+++ Scrimba/BlackJack_Game/index.js
@@ -1,7 +1,7 @@
 // 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 // 2. Create a variable, sum, and set it to the sum of the two cards
-let firstCard = 10;
-let secondCard = 9;
-sum = firstCard + secondCard;
-console.log(sum);
+let firstCard = 10
+let secondCard = 9
+sum = firstCard + secondCard
+console.log(sum)
