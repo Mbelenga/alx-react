0c7e8171956fbf4540e20200a5d4b7995ed49619
Mbelenga <mbelengaterence@gmail.com>
2025-02-20 00:06:01 +0300

    creating another variable and assigning it

--- Scrimba/BlackJack_Game/index.js
+++ Scrimba/BlackJack_Game/index.js
@@ -2,6 +2,7 @@ let firstCard = 10
 let secondCard = 7
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
@@ -18,3 +19,4 @@ console.log(hasBlackJack)
 // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
+// 1. Create a variable called isAlive and assign it to true
