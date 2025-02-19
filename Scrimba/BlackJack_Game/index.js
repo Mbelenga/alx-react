3a170b3114dac344e705ac9de945718431cf4317
Mbelenga <mbelengaterence@gmail.com>
2025-02-19 18:50:59 +0300

    updated

--- Scrimba/BlackJack_Game/index.js
+++ Scrimba/BlackJack_Game/index.js
@@ -3,11 +3,9 @@ let secondCard = 7
 let sum = firstCard + secondCard
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
-}
-else if (sum === 21) {
+} else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
-}
-else {
+} else {
     console.log("You're out of the game! 😭")
 }
 
