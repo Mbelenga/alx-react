7577beaf2bfba33ee89dc72e68c9823c95712f52
Mbelenga <mbelengaterence@gmail.com>
2025-02-21 00:19:36 +0300

    challenge

--- Scrimba/BlackJack_Game/index.js
+++ Scrimba/BlackJack_Game/index.js
@@ -4,6 +4,7 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
 
