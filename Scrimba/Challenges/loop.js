aba22e92bfab598d7df1d77ab898eb72ba7f5a4c
Mbelenga <mbelengaterence@gmail.com>
2025-02-28 01:54:34 +0300

    done

--- Scrimba/Challenges/loop.js
+++ Scrimba/Challenges/loop.js
@@ -1,11 +1,5 @@
 let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
-
-/* Use a for loop to log the following to the console:
-
-The 5 largest countries in the world:
-- China
-- India
-- United States
-- Indinesia
-- Pakistan
-*/
+console.log('The 5 largest countries in the world:')
+for (let index = 0; index < largeCountries.length; index++) {
+    console.log("- " + largeCountries[index] )
+}
