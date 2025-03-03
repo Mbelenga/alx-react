5be483c7ae524591161896e105b63f51d60654b7
Mbelenga <mbelengaterence@gmail.com>
2025-03-03 03:32:26 +0300

    trying to solve the challenge

--- Scrimba/Challenges/push.js
+++ Scrimba/Challenges/push.js
@@ -1,6 +1,7 @@
 let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
 let addCountries = largeCountries.push(China, Pakistan)
 console.log(addCountries);
+console.log(largeCountries)
 
 // You need to help me fixup the largeCountries array so that 
 // China and Pakistan are added back into their respective places
