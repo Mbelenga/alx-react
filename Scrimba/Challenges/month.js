de7068bff404d012dffa428f9b498c8b0d9126e3
Mbelenga <mbelengaterence@gmail.com>
2025-03-03 03:55:16 +0300

    fixed the small error

--- Scrimba/Challenges/month.js
+++ Scrimba/Challenges/month.js
@@ -1,4 +1,4 @@
 let dayOfMonth = 13
 let weekday = "Friday"
-if ( dayofMonth === 13 && weekday === "Friday")
+if ( dayOfMonth === 13 && weekday === "Friday")
     console.log("😱 It's Friday the 13th!")
