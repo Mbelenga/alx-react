dfe7ac0b8b58ee0dc175541da3fe2ab017276597
Mbelenga <mbelengaterence@gmail.com>
2025-03-03 03:50:34 +0300

    understood how shift unshift pop and push works

--- Scrimba/Challenges/push.js
+++ Scrimba/Challenges/push.js
@@ -4,8 +4,3 @@ largeCountries.push('Pakistan')
 largeCountries.shift()
 largeCountries.unshift('China')
 console.log(largeCountries)
-// You need to help me fixup the largeCountries array so that 
-// China and Pakistan are added back into their respective places
-
-// Use push() & pop() and their counterparts unshift() & shift()
-// Google how to use unshift() and shift()
