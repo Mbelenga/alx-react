44bca767e5b131f59943626177f26460c10a4ced
Mbelenga <mbelengaterence@gmail.com>
2025-03-03 03:50:01 +0300

    using the logical && operator

--- Scrimba/Challenges/month.js
+++ Scrimba/Challenges/month.js
@@ -1,5 +1,4 @@
 let dayOfMonth = 13
 let weekday = "Friday"
-
-// If it is Friday the 13th, log out this spooky face: 😱
-// Use the logical "AND operator" -> &&
+if ( dayofMonth === 13 && weekday === "Friday")
+    console.log("😱 It's Friday the 13th!")
